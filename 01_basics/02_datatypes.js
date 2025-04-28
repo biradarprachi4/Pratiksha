1de58cc6c3f5c83bca688988c33270f201e9643e
biradarprachi4 <biradarprachi4@gmail.com>
2025-04-28 21:32:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict" //treat all js code as newer version
+// alert(9+9); //we are using node.js not browser
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+console. log(typeof undefined); // undefined
+console. log(typeof null); // object
