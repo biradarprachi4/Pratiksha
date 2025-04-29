562c8cb65af5b49cc6bf87fc8311540e5ecbef4d
biradarprachi4 <biradarprachi4@gmail.com>
2025-04-29 21:38:40 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,15 @@
+let score = "33" ;
+let isLoggedIn= "pratiksha";
+//undefined is also output nan
+//null output 0
+// console.log(typeof score);
+
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof isLoggedIn);
+
+// 0 => false  1=> true 
+// "" =>false  "pratiksha" true 
